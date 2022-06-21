5e1f6adb3ff22dc2f8feba08f85505a17f90846b
Anthony Connell <toco85@gmail.com>
2022-06-20 20:58:27 -0400

    Final Push

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import App from './App'
+ import userEvent from '@testing-library/user-event' 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+ describe('test for the button', () =>{
+  test("button changes color when clicked", () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+  
+    userEvent.click(colorBtn)
+  
+    expect(colorBtn).toHaveStyle({ backgroundColor: "blue" })
+    expect(colorBtn.textContent).toBe("Change button color to green")
+  })
+}) 
+
+test("checkbox disables button when first clicked, then enables button on second click", () => {
+  render(<App />)
+  const colorBtn = screen.getByRole("button")
+  const checkbox = screen.getByRole("checkbox")
+
+  userEvent.click(checkbox)
+  expect(colorBtn).toBeDisabled()
+
+  userEvent.click(checkbox)
+  expect(colorBtn).toBeEnabled()
+})
+
